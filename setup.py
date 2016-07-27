23365fa27d8a61bfc6e1e9018b77bce40bb9a7f3
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-07-27 09:42:42 +0300

    Move to pbr to simplify things

--- setup.py
+++ setup.py
@@ -5,43 +5,58 @@
 import setuptools
 
 
-REQUIREMENTS_FILE = "requirements.txt"
+try:
+    import multiprocessing
+    assert multiprocessing
+except ImportError:
+    pass
 
 
-def get_install_requires():
-    import pip
-
-    try:
-        requirements = list(pip.req.parse_requirements(REQUIREMENTS_FILE))
-    except Exception:
-        import pip.download
-
-        requirements = list(pip.req.parse_requirements(
-            REQUIREMENTS_FILE, session=pip.download.PipSession()))
-
-    reqs, links = [], []
-    for item in requirements:
-        if getattr(item, "url", None):
-            links.append(str(item.url))
-        if getattr(item, "link", None):
-            links.append(str(item.link))
-        if item.req:
-            reqs.append(str(item.req))
-
-    return reqs, links
-
-
-requires, links = get_install_requires()
 setuptools.setup(
-    name="",
-    version="",
-    author="",
-    author_email="",
-    maintainer="",
-    maintainer_email="",
-    license="",
-    description="",
-    long_description="",
-    packages=[],
-    install_requires=requires,
-    dependency_links=links)
+    setup_requires=["pbr>=1.8"],
+    pbr=True
+)
+# REQUIREMENTS_FILE = "requirements.txt"
+
+
+# def get_install_requires():
+#     import pip
+
+#     try:
+#         requirements = list(pip.req.parse_requirements(REQUIREMENTS_FILE))
+#     except Exception:
+#         import pip.download
+
+#         requirements = list(pip.req.parse_requirements(
+#             REQUIREMENTS_FILE, session=pip.download.PipSession()))
+
+#     reqs, links = [], []
+#     for item in requirements:
+#         if getattr(item, "url", None):
+#             links.append(str(item.url))
+#         if getattr(item, "link", None):
+#             links.append(str(item.link))
+#         if item.req:
+#             reqs.append(str(item.req))
+
+#     return reqs, links
+
+
+# requires, links = get_install_requires()
+# setuptools.setup(
+#     name="cephlcm",
+#     version="0.1",
+#     author="Mirantis",
+#     author_email="",
+#     maintainer="Mirantis",
+#     maintainer_email="",
+#     license="",
+#     description="",
+#     long_description="",
+#     data_files=[
+#         ("etc/cephlcm/", ["configs/defaults.toml"])
+#     ],
+#     packages=setuptools.find_packages(exclude=["tests"]),
+#     install_requires=requires,
+#     dependency_links=links
+# )
