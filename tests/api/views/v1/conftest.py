a47cce43940c8272956de05551ce2484125a83ad
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-08-01 14:38:17 +0300

    Small test refactoring

---
+++ tests/api/views/v1/conftest.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""Fixtures for API V1."""
+
+
+import flask.testing
+import pytest
+
+from cephlcm import api
+
+
+class JsonApiClient(flask.testing.FlaskClient):
+
+    AUTH_URL = None
+    LOGIN = None
+    PASSWORD = None
+
+    def __init__(self, *args, **kwargs):
+        super(JsonApiClient, self).__init__(*args, **kwargs)
+        self.auth_token = None
+
+    def login(self, login=None, password=None):
+        login = login or self.LOGIN
+        password = password or self.PASSWORD
+        response = self.post(
+            self.AUTH_URL, data={"username": login, "password": password})
+
+        if 200 <= response.status_code < 299:
+            self.auth_token = response.json["id"]
+
+        return response
+
+    def logout(self, login=None, password=None):
+        response = self.delete(self.AUTH_URL)
+
+        if 200 <= response.status_code < 299:
+            self.auth_token = None
+
+        return response
+
+    def open(self, *args, **kwargs):
+        data = kwargs.get("data")
+        if data is not None and not kwargs.get("content_type"):
+            kwargs["data"] = flask.json.dumps(data)
+            kwargs["content_type"] = "application/json"
+
+        if self.auth_token:
+            self.install_token(kwargs)
+
+        return super(JsonApiClient, self).open(*args, **kwargs)
+
+    def install_token(self, kwargs):
+        headers = dict(kwargs.pop("headers", []))
+        headers["Authorization"] = self.auth_token
+
+        kwargs["headers"] = sorted(headers.items())
+
+
+@pytest.yield_fixture
+def app(configure_model):
+    application = api.create_application()
+    application.testing = True
+    application.test_client_class = JsonApiClient
+
+    with application.app_context():
+        yield application
+
+
+@pytest.fixture
+def client_v1(client):
+    client.AUTH_URL = "/v1/auth/"
+
+    return client
+
+
+@pytest.yield_fixture
+def sudo_client_v1(app, sudo_user):
+    with app.test_client() as client:
+        client.AUTH_URL = "/v1/auth/"
+        client.login("sudo", "sudo")
+
+        yield client
--- tests/conftest.py
+++ tests/conftest.py
@@ -12,8 +12,6 @@ try:
 except ImportError:
     import mock
 
-import flask.json
-import flask.testing
 import pymongo
 import pytest
 
@@ -24,53 +22,6 @@ from cephlcm.common.models import generic
 from cephlcm.common.models import role
 from cephlcm.common.models import user
 from cephlcm.common import wrappers
-
-
-class JsonApiClient(flask.testing.FlaskClient):
-
-    AUTH_URL = None
-    LOGIN = None
-    PASSWORD = None
-
-    def __init__(self, *args, **kwargs):
-        super(JsonApiClient, self).__init__(*args, **kwargs)
-        self.auth_token = None
-
-    def login(self, login=None, password=None):
-        login = login or self.LOGIN
-        password = password or self.PASSWORD
-        response = self.post(
-            self.AUTH_URL, data={"username": login, "password": password})
-
-        if 200 <= response.status_code < 299:
-            self.auth_token = response.json["id"]
-
-        return response
-
-    def logout(self, login=None, password=None):
-        response = self.delete(self.AUTH_URL)
-
-        if 200 <= response.status_code < 299:
-            self.auth_token = None
-
-        return response
-
-    def open(self, *args, **kwargs):
-        data = kwargs.get("data")
-        if data is not None and not kwargs.get("content_type"):
-            kwargs["data"] = flask.json.dumps(data)
-            kwargs["content_type"] = "application/json"
-
-        if self.auth_token:
-            self.install_token(kwargs)
-
-        return super(JsonApiClient, self).open(*args, **kwargs)
-
-    def install_token(self, kwargs):
-        headers = dict(kwargs.pop("headers", []))
-        headers["Authorization"] = self.auth_token
-
-        kwargs["headers"] = sorted(headers.items())
 
 
 def have_mocked(request, *mock_args, **mock_kwargs):
@@ -141,23 +92,6 @@ def configure_model(mongo_db_name, pymongo_connection):
     generic.configure_models(None)
 
 
-@pytest.yield_fixture
-def app(configure_model):
-    application = api.create_application()
-    application.testing = True
-    application.test_client_class = JsonApiClient
-
-    with application.app_context():
-        yield application
-
-
-@pytest.fixture
-def client_v1(client):
-    client.AUTH_URL = "/v1/auth/"
-
-    return client
-
-
 @pytest.fixture
 def smtp(request, monkeypatch):
     sendmail_mock = mock.MagicMock()
@@ -196,12 +130,3 @@ def sudo_user(sudo_role):
         "Almighty Sudo",
         [sudo_role.model_id]
     )
-
-
-@pytest.yield_fixture
-def sudo_client_v1(app, sudo_user):
-    with app.test_client() as client:
-        client.AUTH_URL = "/v1/auth/"
-        client.login("sudo", "sudo")
-
-        yield client
