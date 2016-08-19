0f6ac401010591d9de58fee6827eafb78c22b871
Nikolay Bogdanov <nbogdanov@mirantis.com>
2016-08-19 17:10:14 +0300

    User creation/editing modal dialog created

--- ui/app/admin/users.ts
+++ ui/app/admin/users.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Modal } from '../bootstrap';
 import { DataService } from '../services/data';
 
 @Component({
-  templateUrl: './app/templates/users.html'
+  templateUrl: './app/templates/users.html',
+  directives: [Modal]
 })
 export class UsersComponent {
   users: any[] = [];
-  constructor(private data: DataService) {
+  roles: any[] = [];
+  newUser: any = {data: {}};
+
+  constructor(private data: DataService, private modal: Modal) {
     this.data.user().findAll({})
       .then((users: any) => this.users = users.items);
+    this.data.role().findAll({})
+      .then((roles: any) => this.roles = roles.items);
   }
 
-  edit(userId: string) {
+  editUser(user: any = null) {
+    this.newUser = _.isNull(user) ? {data: {}} : user;
+    this.modal.show();
   }
 }
