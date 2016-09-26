4137d0ab5507c34e8300852be5c07de1100c36a7
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-09-26 10:47:01 +0300

    Remove pbr from cephlcm controller

--- backend/controller/setup.py
+++ backend/controller/setup.py
@@ -5,14 +5,41 @@
 import setuptools
 
 
-try:
-    import multiprocessing
-    assert multiprocessing
-except ImportError:
-    pass
+REQUIREMENTS = (
+    "cephlcm-common==0.1.0-alpha",
+    "daemonocle",
+    "click"
+)
 
 
 setuptools.setup(
-    setup_requires=["pbr>=1.10"],
-    pbr=True
+    name="cephlcm-controller",
+    description="Ceph Lifecycle Management controller service",
+    long_description="",  # TODO
+    version="0.1.0-alpha",
+    author="Sergey Arkhipov",
+    author_email="sarkhipov@mirantis.com",
+    mainainer="Sergey Arkhipov",
+    maintainer_email="sarkhipov@mirantis.com",
+    license="Apache2",
+    url="https://github.com/Mirantis/ceph-lcm",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.4",
+    install_requires=REQUIREMENTS,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "cephlcm-controller = cephlcm_controller.daemon:main",
+            "cephlcm-inventory = cephlcm_controller.inventory:main"
+        ]
+    },
+    classifiers=(
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    )
 )
