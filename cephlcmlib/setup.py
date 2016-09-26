294610cb25effae97b0ba32d42dfa2f0366a2793
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-09-26 10:27:59 +0300

    Remove pbr from cephlcmlib

--- cephlcmlib/setup.py
+++ cephlcmlib/setup.py
@@ -5,14 +5,35 @@
 import setuptools
 
 
-try:
-    import multiprocessing
-    assert multiprocessing
-except ImportError:
-    pass
+REQUIREMENTS = (
+    "requests[socks]",
+    "six"
+)
 
 
 setuptools.setup(
-    setup_requires=["pbr>=1.10"],
-    pbr=True
+    name="cephlcmlib",
+    description="Ceph Lifecycle Management client library",
+    long_description="",  # TODO
+    version="0.1.0-alpha",
+    author="Sergey Arkhipov",
+    author_email="sarkhipov@mirantis.com",
+    mainainer="Sergey Arkhipov",
+    maintainer_email="sarkhipov@mirantis.com",
+    license="Apache2",
+    url="https://github.com/Mirantis/ceph-lcm",
+    packages=setuptools.find_packages(),
+    python_requires=">=2.7",
+    install_requires=REQUIREMENTS,
+    zip_safe=True,
+    classifiers=(
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    )
 )
