586c64ae65149e81706139f405a6f9d82891c7d8
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-10-10 08:32:18 +0300

    Plugin to remove OSD host

--- plugins/playbook/remove_osd/setup.py
+++ plugins/playbook/remove_osd/setup.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Setup script for Add OSD plugin."""
+"""Setup script for Remove OSD plugin."""
 
 
 import setuptools
 
 
 setuptools.setup(
-    name="cephlcm-plugin-playbook-add-osd",
-    description="Add OSD plugin for CephLCM",
+    name="cephlcm-plugin-playbook-remove-osd",
+    description="Remove OSD plugin for CephLCM",
     version="0.1.0a0",
     author="Sergey Arkhipov",
     author_email="sarkhipov@mirantis.com",
     url="https://github.com/Mirantis/ceph-lcm",
     packages=setuptools.find_packages(),
     entry_points={
-        "cephlcm.playbooks": ["add_osd = cephlcm_add_osd.plugin:AddOSD"]
+        "cephlcm.playbooks": [
+            "remove_osd = cephlcm_remove_osd.plugin:RemoveOSD"
+        ]
     },
     python_requires=">= 3.4",
     include_package_data=True,
     package_data={
-        "cephlcm_add_osd": [
+        "cephlcm_remove_osd": [
             "config.yaml",
             "playbook.yaml"
         ]
