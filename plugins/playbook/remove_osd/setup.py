77280eb796be751d81a6eb7f73cd933afb0a227c
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-10-31 17:12:20 +0300

    Move cephlcm_remove_osd to shrimp_remove_osd

--- plugins/playbook/remove_osd/setup.py
+++ plugins/playbook/remove_osd/setup.py
@@ -7,22 +7,22 @@ import setuptools
 
 
 setuptools.setup(
-    name="cephlcm-plugin-playbook-remove-osd",
-    description="Remove OSD plugin for CephLCM",
+    name="shrimp-plugin-playbook-remove-osd",
+    description="Remove OSD plugin for Shrimp",
     version="0.1.0",
     author="Sergey Arkhipov",
     author_email="sarkhipov@mirantis.com",
     url="https://github.com/Mirantis/ceph-lcm",
     packages=setuptools.find_packages(),
     entry_points={
-        "cephlcm.playbooks": [
-            "remove_osd = cephlcm_remove_osd.plugin:RemoveOSD"
+        "shrimp.playbooks": [
+            "remove_osd = shrimp_remove_osd.plugin:RemoveOSD"
         ]
     },
     python_requires=">= 3.4",
     include_package_data=True,
     package_data={
-        "cephlcm_remove_osd": [
+        "shrimp_remove_osd": [
             "config.yaml",
             "playbook.yaml"
         ]
