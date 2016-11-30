ef4a8a4c8d65013d381506b81f393b8f41eb9683
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-11-30 12:56:27 +0300

    Update diskutils to support journal/data separation

--- backend/common/decapod_common/diskutils.py
+++ backend/common/decapod_common/diskutils.py
@@ -17,13 +17,68 @@
 import posixpath
 
 
-def get_devices(server):
+def get_devices(server, dev_prefix=True):
     mounts = {mount["device"] for mount in server.facts["ansible_mounts"]}
     mounts = {posixpath.basename(mount) for mount in mounts}
 
     devices = []
     for name, data in server.facts["ansible_devices"].items():
         if not (set(data["partitions"]) & mounts):
-            devices.append(posixpath.join("/", "dev", name))
+            name = get_dev_name(name, dev_prefix)
+            devices.append(name)
 
     return devices
+
+
+def get_data_journal_pairs(server, dev_prefix=True):
+    return list(get_data_journal_pairs_iter(server, dev_prefix))
+
+
+def get_data_journal_pairs_iter(server, dev_prefix=True):
+    devices = get_devices(server, False)
+    devices = set(devices)
+    size_sorter = dev_size_sorter(server)
+
+    fast_disks = get_flash_disks(server, devices)
+    slow_disks = devices - fast_disks
+    fast_disks.sort(key=size_sorter, reverse=True)
+    slow_disks.sort(key=size_sorter, reverse=True)
+    while fast_disks and slow_disks:
+        yield {
+            "data": get_dev_name(slow_disks.pop(0), dev_prefix),
+            "journal": get_dev_name(fast_disks.pop(0), dev_prefix)
+        }
+
+    all_disks = set(fast_disks) | set(slow_disks)
+    all_disks = sorted(all_disks, key=size_sorter, reverse=True)
+    while len(all_disks) > 1:
+        yield {
+            "data": get_dev_name(all_disks.pop(0), dev_prefix),
+            "journal": get_dev_name(all_disks.pop(-1), dev_prefix)
+        }
+
+
+def get_flash_disks(server, devices):
+    facts = server.facts["ansible_devices"]
+    devices = filter(lambda dev: facts[dev]["rotational"] == "0", devices)
+    devices = set(devices)
+
+    return devices
+
+
+def get_dev_name(device, dev_prefix=True):
+    if dev_prefix:
+        return posixpath.join("/", "dev", device)
+
+    return device
+
+
+def dev_size_sorter(server):
+    facts = server.facts["ansible_devices"]
+
+    def sorter(device):
+        sectors = int(facts[device]["sectors"])
+        size = int(facts[device]["sectorsize"])
+        return sectors * size
+
+    return sorter
