759510ee135060c3ea0d478d15423082ecb2de4d
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-12-02 10:15:10 +0300

    Propagate hints to add_mon

--- plugins/playbook/__template__/{{cookiecutter.plugin_name}}/{{cookiecutter.package}}/plugin.py
+++ plugins/playbook/__template__/{{cookiecutter.plugin_name}}/{{cookiecutter.package}}/plugin.py
@@ -58,19 +58,19 @@ class {{ cookiecutter.plugin_class_name }}(playbook_plugin.CephAnsiblePlaybook):
         if exc_value:
             raise exc_value
 
-    def make_playbook_configuration(self, cluster, servers):
-        global_vars = self.make_global_vars(cluster, servers)
-        inventory = self.make_inventory(cluster, servers)
+    def make_playbook_configuration(self, cluster, servers, hints):
+        global_vars = self.make_global_vars(cluster, servers, hints)
+        inventory = self.make_inventory(cluster, servers, hints)
 
         return global_vars, inventory
 
-    def make_global_vars(self, cluster, servers):
-        result = super().make_global_vars(cluster, servers)
+    def make_global_vars(self, cluster, servers, hints):
+        result = super().make_global_vars(cluster, servers, hints)
 
         return result
 
-    def make_inventory(self, cluster, servers):
-        groups = self.get_inventory_groups(cluster, servers)
+    def make_inventory(self, cluster, servers, hints):
+        groups = self.get_inventory_groups(cluster, servers, hints)
         inventory = {"_meta": {"hostvars": {}}}
 
         for name, group_servers in groups.items():
@@ -83,7 +83,7 @@ class {{ cookiecutter.plugin_class_name }}(playbook_plugin.CephAnsiblePlaybook):
 
         return inventory
 
-    def get_inventory_groups(self, cluster, servers):
+    def get_inventory_groups(self, cluster, servers, hints):
         cluster_servers = server.ServerModel.cluster_servers(cluster.model_id)
         cluster_servers = {item._id: item for item in cluster_servers}
 
--- plugins/playbook/add_mon/decapod_plugin_playbook_add_mon/plugin.py
+++ plugins/playbook/add_mon/decapod_plugin_playbook_add_mon/plugin.py
@@ -55,21 +55,21 @@ class AddMon(playbook_plugin.CephAnsiblePlaybook):
         if cluster.configuration.changed:
             cluster.save()
 
-    def make_playbook_configuration(self, cluster, servers):
-        global_vars = self.make_global_vars(cluster, servers)
-        inventory = self.make_inventory(cluster, servers)
+    def make_playbook_configuration(self, cluster, servers, hints):
+        global_vars = self.make_global_vars(cluster, servers, hints)
+        inventory = self.make_inventory(cluster, servers, hints)
 
         return global_vars, inventory
 
-    def make_global_vars(self, cluster, servers):
-        result = super().make_global_vars(cluster, servers)
+    def make_global_vars(self, cluster, servers, hints):
+        result = super().make_global_vars(cluster, servers, hints)
 
         result["ceph_facts_template"] = pkg_resources.resource_filename(
             "decapod_common", "facts/ceph_facts_module.py.j2")
 
         return result
 
-    def make_inventory(self, cluster, servers):
+    def make_inventory(self, cluster, servers, hints):
         groups = self.get_inventory_groups(cluster, servers)
         inventory = {"_meta": {"hostvars": {}}}
 
@@ -83,7 +83,7 @@ class AddMon(playbook_plugin.CephAnsiblePlaybook):
 
         return inventory
 
-    def get_inventory_groups(self, cluster, servers):
+    def get_inventory_groups(self, cluster, servers, hints):
         cluster_servers = server.ServerModel.cluster_servers(cluster.model_id)
         cluster_servers = {item._id: item for item in cluster_servers}
 
