252549520145c0a6a2ccf7a55527b31441eab097
Sergey Arkhipov <sarkhipov@mirantis.com>
2016-12-05 16:05:58 +0300

    Add script for migration to native ttl indexes

--- backend/migration/decapod_migration/scripts/0003_native_ttl_index.py
+++ backend/migration/decapod_migration/scripts/0003_native_ttl_index.py
@@ -13,20 +13,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-This migration creates unique TTL index for tokens.
+This migration creates native TTL indexes.
 """
 
 
 from decapod_api import wsgi
 from decapod_common.models import db
 from decapod_common.models import generic
+from decapod_common.models import password_reset
+from decapod_common.models import task
 from decapod_common.models import token
 
 
 with wsgi.application.app_context():
     generic.configure_models(db.MongoDB())
-    token.collection().create_index(
+
+    password_reset.PasswordReset.collection().create_index(
         "expires_at",
         expireAfterSeconds=0,
-        name="index_token_ttl"
-    )
+        name="index_pwreset_ttl")
+    task.Task.collection().create_index(
+        task.TTL_FIELDNAME,
+        expireAfterSeconds=0,
+        name="index_task_ttl")
+    token.TokenModel.collection().create_index(
+        "expires_at",
+        expireAfterSeconds=0,
+        name="index_token_ttl")
